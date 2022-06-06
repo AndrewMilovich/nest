82ffcf13f83996c3466c6945799f76d543fb6cdf
AndrewMilovich <triplle@ukr.net>
2022-06-06 16:51:46 +0300

    add users

---
+++ src/users/users.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from "@nestjs/common";
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  getUsers(): CreateUserDto[] {
+    return this.userService.getAll();
+  }
+
+  @Get('/:id')
+  getOneUser(@Param('id') id: string): CreateUserDto {
+    return this.userService.getUsersById(id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post()
+  createUser(@Body() userDto: CreateUserDto): CreateUserDto {
+    return this.userService.createUser(userDto);
+  }
+
+@HttpCode(HttpStatus.OK)
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    this.userService.deleteUserById(id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Put(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() userDto: Partial<CreateUserDto>,
+  ): CreateUserDto {
+    return this.userService.updateUserById(id, userDto);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  private users = [];
+
+  getAll(): CreateUserDto[] {
+    return this.users;
+  }
+
+  getUsersById(id: string): CreateUserDto {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(user: CreateUserDto): CreateUserDto {
+    const createdUser={ ...user, id: new Date().valueOf() }
+    this.users.push(createdUser);
+    return createdUser;
+  }
+
+  deleteUserById(id: string): void {
+    const index = this.users.find((value) => value.id === id);
+    this.users.splice(index, 1);
+  }
+
+  updateUserById(
+    id: string,
+    user: Partial<CreateUserDto>
+  ): CreateUserDto {
+    const updateUser = this.users.find((value) => (value.id = id));
+    return Object.assign(updateUser, user);
+  }
+}
