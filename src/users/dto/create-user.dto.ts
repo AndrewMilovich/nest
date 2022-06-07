48c759159e406c6e21771fbfe7fad1eaeee05768
AndrewMilovich <triplle@ukr.net>
2022-06-07 16:54:31 +0300

    add posts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,20 @@
+import { CreatePostDto } from '../../posts/dto/create-post.dto';
+
 export class CreateUserDto {
-  public username: string;
+  public name: string;
   public email: string;
   public age: number;
+  public city: string;
+  public status: boolean;
   readonly password: string;
+  public posts: CreatePostDto[];
 }
+// id       Int       @id @default(autoincrement())
+// email    String    @unique
+// name     String
+// age      Int
+// city     String?
+//   password String
+// status   Boolean
+// posts    Post[]
+// comments Comment[]
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,31 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from '../core/prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  private users = [];
+  // private users = [];
+  constructor(private prismaService: PrismaService) {}
 
-  getAll(): CreateUserDto[] {
-    return this.users;
+  getAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
   }
 
-  getUsersById(id: string): CreateUserDto {
-    return this.users.find((user) => user.id === id);
+  getUsersById(id: string): Promise<User> {
+    return this.prismaService.user.findUnique({ where: { id: Number(id) } });
   }
 
-  createUser(user: CreateUserDto): CreateUserDto {
-    const createdUser = { ...user, id: new Date().valueOf() };
-    this.users.push(createdUser);
-    return createdUser;
+  createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({ data });
   }
 
-  deleteUserById(id: string): void {
-    const index = this.users.findIndex((value) => value.id === id);
-    this.users.splice(index, 1);
-  }
-
-  updateUserById(id: string, user: Partial<CreateUserDto>): CreateUserDto {
-    const updateUser = this.users.find((value) => (value.id == id));
-    return Object.assign(updateUser, user);
+  // deleteUserById(id: string): void {
+  //   const index = this.users.findIndex((value) => value.id === id);
+  //   this.users.splice(index, 1);
+  // }
+  //
+  updateUser(id: string, userData: Prisma.UserUpdateInput): Promise<User> {
+    return this.prismaService.user.update({
+      where: { id: Number(id) },
+      data: { name: userData.name, city: userData.city },
+    });
   }
 }
